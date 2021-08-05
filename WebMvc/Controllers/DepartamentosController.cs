83119bfb80b765ce4642660744e2a39b97eff0f7
Wellington Junior <junior.mac91@gmail.com>
2021-08-04 21:42:44 -0300

    First Model-Controller-View - Department

---
+++ WebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+
+            list.Add(new Departamento { Id = 1, Nome = "Eletrônicos" });
+            list.Add(new Departamento { Id = 2, Nome = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
--- WebMvc/Models/ViewModels/ErrorViewModel.cs
+++ WebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace WebMvc.Models
+namespace WebMvc.Models.ViewModels
 {
     public class ErrorViewModel
     {
