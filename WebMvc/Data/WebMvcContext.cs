0b0fa35362479f2a659e2f5c25e6080fb1dfb6ca
Wellington Junior <junior.mac91@gmail.com>
2021-08-04 22:03:53 -0300

    Crud Scaffolding

---
+++ WebMvc/Data/WebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebMvc.Models
+{
+    public class WebMvcContext : DbContext
+    {
+        public WebMvcContext (DbContextOptions<WebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMvc.Models.Departamento> Departamento { get; set; }
+    }
+}
--- WebMvc/Startup.cs
+++ WebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebMvc.Models;
 
 namespace WebMvc
 {
@@ -33,6 +35,9 @@ namespace WebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
